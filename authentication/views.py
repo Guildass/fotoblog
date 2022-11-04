b45286cbf33adeccf981f5cf57862b1e6930f3ae
Guildass <Guildass@users.noreply.github.com>
2022-11-04 11:26:05 +0100

    Ajout page d'inscription

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,22 @@
+from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views.generic import View
 
 from authentication import forms
+
+
+def signup_page(request):
+    form = forms.SignupForm(request.POST)
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form': form})
+
 
 
 # def login_page(request):
@@ -49,6 +62,6 @@ from authentication import forms
 #         return render(request, self.template_name, context={'form': form, 'message': message})
 #
 #
-def logout_user(request):
-    logout(request)
-    return redirect('login')
+# def logout_user(request):
+#     logout(request)
+#     return redirect('login')
